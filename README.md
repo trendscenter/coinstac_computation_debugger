ab33d0bc87a75bd0c4f99ecf1974e8dea14c4d09
Sunitha Basodi <sbasodi1@URS6418SBASODI1.local>
2024-04-05 11:56:15 -0400

    Adding project files

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # coinstac_computation_debugger
-This software help in real time debugging coinstac computation code without any modification.
+This software help in real time debugging COINSTAC computation code without any modification.
+https://coinstac.org/ 
+
+## Usage 
+Refer to "debugger_usage_example.py" for usage instructions
--- src/coinstac_computation_debugger/debugger_usage_example.py
+++ src/coinstac_computation_debugger/debugger_usage_example.py
@@ -2,8 +2,9 @@
 # -*- coding: utf-8 -*-
 
 """
-This script includes the local computations for linear mixed effects model
-with decentralized statistic calculation
+This script can be copied in your repository and  paste it in the folder
+where lcoal and remote scripts exists; modify variables with TODO comments
+and run/debug using this script
 """
 import os
 import warnings
@@ -12,26 +13,26 @@ with warnings.catch_warnings():
     warnings.simplefilter('ignore')
 from local import *
 from remote import *
-from coinstac_computation_debugger import *
+from computation_debugger import *
 
 
 def new_debugger():
-    TEST_DIR = "../test/eimhear-anthony-data/"
+    TEST_DIR = "../test/test_data_dir_XXXX/" #TODO: replace with test dir path
     DEBUG_DIR = os.path.join(TEST_DIR, "DEBUG_new")
     temp_dir_prefix = "run1"
 
-    num_iterations = 3
 
     NUM_CLIENTS = 2
     dbg= CoinstacComputationDebugger(NUM_CLIENTS, TEST_DIR, DEBUG_DIR)
-    local_calls_list=[local_0, local_1, local_2]
-    remote_calls_list=[remote_0, remote_1, remote_2]
+    local_calls_list=[local_0, local_1, local_2] #TODO: replace with your local calls in order being executed
+    remote_calls_list=[remote_0, remote_1, remote_2]#TODO: replace with your remote calls in order being executed
+    num_iterations = len(local_calls_list)
+
     dbg.run_iterations(os.path.join(TEST_DIR, 'input', 'inputspec-eimhear-anthony.json'), num_iterations , local_calls_list ,remote_calls_list )
 
 
     print("Done processing...")
 
 if __name__ == '__main__':
-    #old_debugger()
     new_debugger()
 
